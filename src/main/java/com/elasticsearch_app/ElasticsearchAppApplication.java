c26b8e953ff60c437bdb5a95239b6c069dd8e1fb
manivannancsmca <manivannancsmca@gmail.com>
2026-07-18 20:24:49 +0530

    first commit

---
+++ src/main/java/com/elasticsearch_app/ElasticsearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticsearch_app/ElasticsearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
