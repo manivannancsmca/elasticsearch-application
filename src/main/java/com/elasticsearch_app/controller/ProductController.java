046679c96fcfd2555746a0582a9ed3d723c60f27
manivannancsmca <manivannancsmca@gmail.com>
2026-07-19 11:11:40 +0530

    product controller created

---
+++ src/main/java/com/elasticsearch_app/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.elasticsearch_app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elasticsearch_app.dto.ProductRequest;
+import com.elasticsearch_app.dto.ProductResponse;
+import com.elasticsearch_app.service.ProductService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.indexProduct(request));
+    }
+}
